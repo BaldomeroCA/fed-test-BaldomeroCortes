a12f78a943ed73b7b620320cf45d44c7afc32e4e
Baldomero Cortés <52608314+BaldomeroCA@users.noreply.github.com>
2022-09-14 13:26:15 -0500

    first commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,41 @@
+var reportsWidget = {
+    options: {
+        containerSelector: '.reports',
+        template: (
+            '{{#.}}' +
+            '<div class="fila">'+
+            '<div class="columna">'+
+                '<article class="reports_item">' +
+                    '<a href="{{cover}}" target="_blank">' +
+                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'+
+                    '</a>' +
+            
+                    '<footer class="reports_docs">' +
+                        '{{#documents}}' +
+                            '<h3 class="reports_title">'+
+                                '<a href="{{url}}" target="_blank">{{title}}</a>'+'<a href="{{url}}" target="_blank">({{file_size}}</a>' + '<a href="{{url}}" target="_blank">{{file_type}})</a>'+
+                            '</h3>' +
+                        '{{/documents}}' +
+                    '</footer>' +
+           
+                '</article>' +
+            '</div>'+
+            '</div>'+
+            '{{/.}}'
+        )
+    },
 
+    init: function() {
+        this.renderReports(reportData || []);
+        
+    },
+
+    renderReports: function(reports) {
+        var inst = this,
+            options = inst.options;
+
+        $(options.containerSelector).html(Mustache.render(options.template, reports));
+    }
+};
+
+reportsWidget.init();
